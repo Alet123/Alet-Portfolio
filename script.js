f5d5f9e0f64514a68bdda3028170578a9e2e7fc1
Alet Jac <82381562+Alet123@users.noreply.github.com>
2025-07-22 17:24:26 +0530

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-// Theme Toggle
-document.addEventListener('DOMContentLoaded', () => {
-  const toggle = document.getElementById('themeToggle');
-  const root = document.documentElement;
+// Theme toggle
+document.addEventListener("DOMContentLoaded", function () {
+  const toggle = document.getElementById("themeToggle");
+  const html = document.documentElement;
 
-  toggle.addEventListener('change', () => {
-    root.setAttribute('data-theme', toggle.checked ? 'light' : 'dark');
+  // Set initial theme
+  if (localStorage.getItem("theme") === "light") {
+    html.setAttribute("data-theme", "light");
+    toggle.checked = true;
+  }
+
+  toggle.addEventListener("change", () => {
+    if (toggle.checked) {
+      html.setAttribute("data-theme", "light");
+      localStorage.setItem("theme", "light");
+    } else {
+      html.setAttribute("data-theme", "dark");
+      localStorage.setItem("theme", "dark");
+    }
   });
 });
